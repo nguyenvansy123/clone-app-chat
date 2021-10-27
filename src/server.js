d9726b4ae5af773b4dfd9b63d16ed041c80f2be6
van sy <nguyenvansy031999@gmail.com>
2021-10-27 21:26:58 +0700

    define model schemas

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+let app = express();
+
+
+var hostName = "localhost";
+var port = 8017;
+
+app.get("/helloworld", (req, res) => {
+    res.send("<h1>hello world</h1>")
+});
+
+app.listen(port, hostName, () => {
+    console.log(`hello , im running at ${hostName}:${port} `);
+});
+
